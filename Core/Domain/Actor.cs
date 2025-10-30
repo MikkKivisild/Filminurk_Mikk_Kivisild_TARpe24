08b24d06c165ec9708c1532514390a604784508d
Opilane TTHK <opilane@tthk.ee>
2025-10-30 08:48:27 +0200

    added actors Domain and Dto.

---
+++ Core/Domain/Actor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public enum Gender
+    {
+        Male, Female
+    }
+    public class Actor
+    {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        List<string> MoviesActedFor { get; set; }
+        public Guid PortraitID { get; set; }
+        public DateTime FirstActed {  get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
