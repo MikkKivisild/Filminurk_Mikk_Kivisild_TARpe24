f6531c6e56daaca0d1bee6fe287a9d4b65081e79
Opilane TTHK <opilane@tthk.ee>
2025-10-30 10:30:18 +0200

    CreateUpdate View Complete and made ActorID in vm optional to use HasValue

--- Filminurk_Mikk_Kivisild_TARpe24/Models/Actors/ActorsCreateUpdateViewModel.cs
+++ Filminurk_Mikk_Kivisild_TARpe24/Models/Actors/ActorsCreateUpdateViewModel.cs
@@ -5,7 +5,7 @@ namespace Filminurk_Mikk_Kivisild_TARpe24.Models.Actors
     public class ActorsCreateUpdateViewModel
     {
         [Key]
-        public Guid ActorID { get; set; }
+        public Guid? ActorID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string NickName { get; set; }
