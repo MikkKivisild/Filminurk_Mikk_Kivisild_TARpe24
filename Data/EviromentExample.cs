bb58fd99fcb9a79456f4d2b82231b9f01c50f8ae
Opilane TTHK <opilane@tthk.ee>
2025-12-05 12:01:22 +0200

    examplefile

---
+++ Data/EviromentExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class EviromentExample //rename class to just "Enviroment"
+    {
+        public static string smtaddress = ""; // server address for sending emails via smtp
+        public static string gmailapppassword = ""; // password for user, that has access to this server (google app password)
+        public static string gmailusername = ""; // usserma,e of email sending user (gmail account)
+    }
+}
--- Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d0c2e63f897c94c57f0727cc8963a817798904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0d3842c6ae9635a0a5938bd8146307e3cb0fd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
