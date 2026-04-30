84e5854f93bb0a8d0714e2c49293898c165b8939
MikkKivisild <159937176+MikkKivisild@users.noreply.github.com>
2026-04-30 14:34:48 +0300

    RChat Controller

---
+++ Filminurk_Mikk_Kivisild_TARpe24/Controllers/RChatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Filminurk_Mikk_Kivisild_TARpe24.Controllers
+{
+    public class RChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
