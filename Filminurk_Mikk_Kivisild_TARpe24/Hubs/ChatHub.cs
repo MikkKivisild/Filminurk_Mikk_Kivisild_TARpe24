1a7783cc30ef4ede84926111166c9b096469272b
MikkKivisild <159937176+MikkKivisild@users.noreply.github.com>
2026-04-30 14:35:11 +0300

    Hub done

---
+++ Filminurk_Mikk_Kivisild_TARpe24/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filminurk_Mikk_Kivisild_TARpe24.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            var userName = Context.User?.Identity?.Name ?? "Unknown";
+            await Clients.All.SendAsync("ReceiveMessage", userName, message);
+        }
+    }
+}
